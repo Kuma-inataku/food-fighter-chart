c6d49fca96eb0e76a7f0a13def90bfebf95aedd8
Kuma-inataku <inataku0121@gmail,com>
2022-06-26 13:34:21 +0900

    bar-chart修正

--- src/components/bar-chart.vue
+++ src/components/bar-chart.vue
@@ -1,15 +1,18 @@
 <template>
-  <Bar
-    :chart-options="chartOptions"
-    :chart-data="chartData"
-    :chart-id="chartId"
-    :dataset-id-key="datasetIdKey"
-    :plugins="plugins"
-    :css-classes="cssClasses"
-    :styles="styles"
-    :width="width"
-    :height="height"
-  />
+  <div>
+    <Bar
+      :chart-options="chartOptions"
+      :chart-data="chartData"
+      :chart-id="chartId"
+      :dataset-id-key="datasetIdKey"
+      :plugins="plugins"
+      :css-classes="cssClasses"
+      :styles="styles"
+      :width="width"
+      :height="height"
+    />
+
+  </div>
 </template>
 
 <script>
@@ -54,12 +57,34 @@ export default {
   data() {
     return {
       chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
+        labels: [ 'MAX鈴木', 'ぞうさんパクパク', 'えびまよ', 'しのけん' ],
+        datasets: [ { data: [0, 0, 0, 0] } ]
       },
       chartOptions: {
         responsive: true
+      },
+      apiKey: process.env.VUE_APP_API_KEY_2,
+    }
+  },
+  async mounted() {
+    for (const fighterIndex in this.chartData.labels) {
+      console.log(this.chartData.labels[fighterIndex]);
+      // TODO: URLオブジェクトのsetメソッドでパラメータ追加. パラメータはdataプロパティ化
+      const videosData = await fetch('https://www.googleapis.com/youtube/v3/search?type=video&part=id,snippet&order=viewCount&q=intitle:大食い+intitle:'+this.chartData.labels[fighterIndex]+'&maxResults=5&key='+this.apiKey);
+      let videos = await videosData.json();
+      console.log(videos.items);
+      for (let videoIndex in videos.items) {
+        const id = videos.items[videoIndex].id.videoId;
+        const videoData = await fetch('https://www.googleapis.com/youtube/v3/videos?part=statistics&id='+id+'&key='+this.apiKey);
+        const video = await videoData.json();
+        console.log(fighterIndex);
+        console.log(this.chartData.datasets);
+        console.log(this.chartData.datasets[0]);
+        console.log(video.items[0].statistics.viewCount);
+        // TODO: datasets[0], data[0]は0~4でループ
+        this.chartData.datasets[0].data[fighterIndex] += parseInt(video.items[0].statistics.viewCount)
       }
+      console.log(this.chartData)
     }
   }
 }
